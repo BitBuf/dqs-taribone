a7aa5216295a72f405a41abbb05ed17ea3d9156f
Dewy <dewy@dewy.dev>
2020-08-14 21:27:23 +0100

    bump 3.2.1

--- src/main/java/dev/dewy/taribone/Taribone.java
+++ src/main/java/dev/dewy/taribone/Taribone.java
@@ -32,7 +32,7 @@ public class Taribone
 {
     public static final String MODID = "taribone";
     public static final String NAME = "Taribone";
-    public static final String VERSION = "3.2.0";
+    public static final String VERSION = "3.2.1";
 
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     public static final File CONIFG_FILE = new File(Minecraft.getMinecraft().gameDir.getAbsolutePath() + "/taribone.json");
